2f4205862ac4f582c8b4dae4363919ed23073c40
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-03-23 20:50:21 +0800

    提出问题 待解决

--- chapter0-0-9/src/main/java/com/goat/chapter009/TranslateAction.java
+++ chapter0-0-9/src/main/java/com/goat/chapter009/TranslateAction.java
@@ -1,5 +1,6 @@
 package com.goat.chapter009;
 
+
 import com.goat.chapter001.util.ConfigUtil;
 import com.goat.chapter001.util.DisplayUtil;
 import com.goat.chapter009.model.MyResult;
@@ -34,41 +35,43 @@ public class TranslateAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
+        MyResult   body;
         final Editor mEditor = anActionEvent.getData(PlatformDataKeys.EDITOR);
         // 获取 百度翻译 API 配置信息
-        Properties properties = ConfigUtil.getProperties("config.properties");
-        String from = properties.getProperty("from");
-        String to = properties.getProperty("to");
-        String appid = properties.getProperty("appid");
-        String sign = properties.getProperty("sign");
-        String salt = String.valueOf(System.currentTimeMillis());
-//        RestTemplate restTemplate = new RestTemplate(); // 不能放在类外面！！！
-        AsyncRestTemplate restTemplate = new AsyncRestTemplate (); // 不能放在类外面！！！
-        // 获取IDEA当前活动编辑器
-        if (null == mEditor) return;
-        SelectionModel model = mEditor.getSelectionModel();
-        // 获取当前活动编辑器中 选中的文本内容
-        final String selectedText = model.getSelectedText();
-        if (TextUtils.isEmpty(selectedText))  return;
-        // 发起请求获取翻译结果
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("q", selectedText);
-        requestBody.add("from",from);
-        requestBody.add("to", to);
-        requestBody.add("appid", appid);
-        requestBody.add("salt", salt);
-        String src = appid + selectedText + salt + sign;
-        requestBody.add("sign", DigestUtils.md5DigestAsHex(src.getBytes(StandardCharsets.UTF_8)));
-        HttpEntity<MultiValueMap> requestEntity = new HttpEntity<>(requestBody, headers);
-        ListenableFuture<ResponseEntity<MyResult>> responseEntity = restTemplate.postForEntity(TRANS_API_HOST, requestEntity, MyResult.class);
-        MyResult body;
         try {
+            Properties  properties = ConfigUtil.getProperties("config.properties");
+            // doit 这里为啥使用 spring的工具类获取config.properties就会报错呢？？？
+            // java.io.FileNotFoundException: class path resource [config.properties] cannot be opened because it does not exist
+            //  PropertiesLoaderUtils.fillProperties(properties,new ClassPathResource("config.properties"));
+            String from = properties.getProperty("from");
+            String to = properties.getProperty("to");
+            String appid = properties.getProperty("appid");
+            String sign = properties.getProperty("sign");
+            String salt = String.valueOf(System.currentTimeMillis());
+            AsyncRestTemplate restTemplate = new AsyncRestTemplate (); // 不能放在类外面！！！
+            // 获取IDEA当前活动编辑器
+            if (null == mEditor) return;
+            SelectionModel model = mEditor.getSelectionModel();
+            // 获取当前活动编辑器中 选中的文本内容
+            final String selectedText = model.getSelectedText();
+            if (TextUtils.isEmpty(selectedText))  return;
+            // 发起请求获取翻译结果
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+            requestBody.add("q", selectedText);
+            requestBody.add("from",from);
+            requestBody.add("to", to);
+            requestBody.add("appid", appid);
+            requestBody.add("salt", salt);
+            String src = appid + selectedText + salt + sign;
+            requestBody.add("sign", DigestUtils.md5DigestAsHex(src.getBytes(StandardCharsets.UTF_8)));
+            HttpEntity<MultiValueMap> requestEntity = new HttpEntity<>(requestBody, headers);
+            ListenableFuture<ResponseEntity<MyResult>> responseEntity = restTemplate.postForEntity(TRANS_API_HOST, requestEntity, MyResult.class);
             body = responseEntity.get().getBody();
         } catch (Exception e) {
             DisplayUtil.showPopupBalloon(mEditor,"error",1000);
-           return;
+            return;
         }
         if(body.getTrans_result() == null) return;
         // 显示翻译结果
